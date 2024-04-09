96fa683473030262aff008a5044402a2abd9c5c6
bc-ania <anna.jezierska@bigcommerce.com>
2024-04-09 15:11:43 +0200

    feat(payment): PI-1576 Refactor the existing checkoutcom components

--- packages/checkoutcom-integration/src/CheckoutcomCustomFormFields.tsx
+++ packages/checkoutcom-integration/src/CheckoutcomCustomFormFields.tsx
@@ -10,11 +10,10 @@ import React, {
 } from 'react';
 
 import { TranslatedString } from '@bigcommerce/checkout/locale';
+import { PaymentFormContext } from '@bigcommerce/checkout/payment-integration-api';
+import { CheckboxFormField, DropdownTrigger, FormField } from '@bigcommerce/checkout/ui';
 
-import { DropdownTrigger } from '../../ui/dropdown';
-import { CheckboxFormField, FormField } from '../../ui/form';
-import { TextFieldForm } from '../creditCard';
-import PaymentContext from '../PaymentContext';
+import TextFieldForm from './checkoutcomFieldsets/TextFieldForm';
 
 interface CheckoutcomAPMFormProps {
     method: PaymentMethod;
@@ -50,17 +49,17 @@ interface SepaCreditor {
 }
 
 const Sepa: FunctionComponent<CheckoutcomAPMFormProps> = ({ method, debtor }) => {
-    const paymentContext = useContext(PaymentContext);
+    const paymentContext = useContext(PaymentFormContext);
     const creditor: SepaCreditor = method.initializationData.sepaCreditor;
 
     useEffect(() => {
-        paymentContext?.disableSubmit(method, true);
+        paymentContext?.paymentForm.disableSubmit(method, true);
 
-        return () => paymentContext?.disableSubmit(method, false);
+        return () => paymentContext?.paymentForm.disableSubmit(method, false);
     }, [paymentContext, method]);
 
     function toggleSubmitButton(isChecked: boolean) {
-        paymentContext?.disableSubmit(method, !isChecked);
+        paymentContext?.paymentForm.disableSubmit(method, !isChecked);
     }
 
     return (
